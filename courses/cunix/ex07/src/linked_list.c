f2187054c2846582fe38f23acc366233ab510b63
Oleksandr Varchenko <alexvarko@gmail.com>
2021-08-16 12:44:01 +0300

    finished ex07

--- courses/cunix/ex07/src/linked_list.c
+++ courses/cunix/ex07/src/linked_list.c
@@ -39,4 +39,54 @@ void list_unshift(node_t **head, void *data){
 }
 
 void *list_pop(node_t **head){
+    node_t *current = *head;
+    node_t *poped = current->next;
+    while (poped->next != NULL)
+    {
+        current = current->next;
+        poped = poped->next;
+    }
+
+    current->next = NULL;
+    free(poped->data);
+    free(poped);
+    return poped;
+}
+
+void *list_shift(node_t **head){
+    node_t *current = *head;
+    *head = current->next;
+    void *data = current->data;
+    free(current);
+    return data;
+}
+
+void *list_remove(node_t **head, int pos){
+    node_t *current = *head;
+    node_t *tmp = *head;
+    current = current->next;
+    for(int i = 0; i < pos-1; i++)
+    {
+        current = current->next;
+        tmp = tmp->next;
+    }
+    void *data = current->data;
+    tmp->next = current->next;
+    free(current);
+    return data;    
+}
+
+void list_print(node_t *head){
+    node_t *current = head;
+    while(current != NULL){
+        printf("%s\n",(char*)(current->data));
+        current = current->next;
+    }
+}
+
+void list_visitor(node_t *head, void(*fp)(void *data)){
+    while(head) {
+        fp(head->data);
+        head = head->next;
+  }
 }

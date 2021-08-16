a53a2ace95227edd833f8e93b9efe681c5c3434e
Oleksandr Varchenko <alexvarko@gmail.com>
2021-08-16 14:20:01 +0300

    style50for ex07

--- courses/cunix/ex07/src/linked_list.c
+++ courses/cunix/ex07/src/linked_list.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-node_t *list_create(void *data){
+node_t *list_create(void *data)
+{
     node_t *head = malloc(sizeof(node_t));
     head->data = data;
     head->next = NULL;
     return head;
 }
 
-void list_destroy(node_t **head, void (*fp)(void *data)){
+void list_destroy(node_t **head, void (*fp)(void *data))
+{
     node_t *current = *head;
     node_t *next;
-    while(current != NULL){
+    while (current != NULL)
+    {
         fp(current->data);
         next = current->next;
         free(current);
@@ -21,9 +24,11 @@ void list_destroy(node_t **head, void (*fp)(void *data)){
     *head = NULL;
 }
 
-void list_push(node_t *head, void *data){
+void list_push(node_t *head, void *data)
+{
     node_t *current = head;
-    while(current->next != NULL){
+    while (current->next != NULL)
+    {
         current = current->next;
     }
     current->next = malloc(sizeof(node_t));
@@ -31,14 +36,16 @@ void list_push(node_t *head, void *data){
     current->next->next = NULL;
 }
 
-void list_unshift(node_t **head, void *data){
+void list_unshift(node_t **head, void *data)
+{
     node_t *new_node = malloc(sizeof(node_t));
     new_node->data = data;
     new_node->next = *head;
     *head = new_node;
 }
 
-void *list_pop(node_t **head){
+void *list_pop(node_t **head)
+{
     node_t *current = *head;
     node_t *poped = current->next;
     while (poped->next != NULL)
@@ -53,7 +60,8 @@ void *list_pop(node_t **head){
     return poped;
 }
 
-void *list_shift(node_t **head){
+void *list_shift(node_t **head)
+{
     node_t *current = *head;
     *head = current->next;
     void *data = current->data;
@@ -61,11 +69,12 @@ void *list_shift(node_t **head){
     return data;
 }
 
-void *list_remove(node_t **head, int pos){
+void *list_remove(node_t **head, int pos)
+{
     node_t *current = *head;
     node_t *tmp = *head;
     current = current->next;
-    for(int i = 0; i < pos-1; i++)
+    for (int i = 0; i < pos - 1; i++)
     {
         current = current->next;
         tmp = tmp->next;
@@ -76,17 +85,21 @@ void *list_remove(node_t **head, int pos){
     return data;    
 }
 
-void list_print(node_t *head){
+void list_print(node_t *head)
+{
     node_t *current = head;
-    while(current != NULL){
-        printf("%s\n",(char*)(current->data));
+    while (current != NULL)
+    {
+        printf("%s\n", (char *)(current->data));
         current = current->next;
     }
 }
 
-void list_visitor(node_t *head, void(*fp)(void *data)){
-    while(head) {
+void list_visitor(node_t *head, void(*fp)(void *data))
+{
+    while (head)
+    {
         fp(head->data);
         head = head->next;
-  }
+    }
 }

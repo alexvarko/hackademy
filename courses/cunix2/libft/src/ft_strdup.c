2a3f7ebf76b932f43eb247c829291adc82054128
Oleksandr Varchenko <alexvarko@gmail.com>
2021-08-17 14:01:12 +0300

    correct style50 for ft_strdup.c

--- courses/cunix2/libft/src/ft_strdup.c
+++ courses/cunix2/libft/src/ft_strdup.c
@@ -2,7 +2,7 @@
 char *ft_strdup(const char *str)
 {   
     int len = ft_strlen(str);
-    char *new_str = (char *)malloc((len +1) * sizeof(char));
+    char *new_str = (char *)malloc((len + 1) * sizeof(char));
     for (int i = 0; i < len; i++)
     {
         new_str[i] = str[i];

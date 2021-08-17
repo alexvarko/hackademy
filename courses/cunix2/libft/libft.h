17b91c49b931b2c827752b2f403fdc8ba8486453
Oleksandr Varchenko <alexvarko@gmail.com>
2021-08-17 18:05:07 +0300

    add ft_isalpha.c

--- courses/cunix2/libft/libft.h
+++ courses/cunix2/libft/libft.h
@@ -6,3 +6,4 @@ int ft_strlen(const char *str);
 int ft_strncmp(const char *str1, const char *str2, size_t n);
 char *ft_strchr(const char *str, int c);
 char *ft_strrchr(const char *str, int c);
+int ft_isalpha(int i);

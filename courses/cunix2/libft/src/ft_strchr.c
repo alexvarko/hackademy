f3713afde2b16769d3bca55dfceb193e53d9d08a
Oleksandr Varchenko <alexvarko@gmail.com>
2021-08-17 15:19:35 +0300

    add ft_strchr.c

---
+++ courses/cunix2/libft/src/ft_strchr.c
@@ -0,0 +1,15 @@
+#include "../libft.h"
+
+char *ft_strchr(const char *str, int c)
+{   
+   char key = c; 
+    while(*str != key)
+    {
+        if (*str == '\0')
+        {
+            return NULL;
+        }
+        str++;
+    }
+    return (char *) str;
+}

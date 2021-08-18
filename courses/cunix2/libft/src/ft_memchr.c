d99ccc308d710327ebf08b0aa0392275d85bbc39
Oleksandr Varchenko <alexvarko@gmail.com>
2021-08-18 16:49:11 +0300

    style correction

--- courses/cunix2/libft/src/ft_memchr.c
+++ courses/cunix2/libft/src/ft_memchr.c
@@ -4,7 +4,7 @@ void *ft_memchr(const void *s, int c, size_t n)
     char *p_s = (char *) s;
     for (size_t i = 0; i < n; i++)
     {
-        if(p_s[i] == (char) c)
+        if (p_s[i] == (char) c)
         {
             return &p_s[i];
         }

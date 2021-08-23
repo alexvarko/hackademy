9d184c1ee8da03cd7d8f58ee1a914a679b1bcd99
Oleksandr Varchenko <alexvarko@gmail.com>
2021-08-23 03:32:32 +0300

    add ft_printf.c, started

---
+++ courses/cunix2/ft_printf/src/ft_printf.c
@@ -0,0 +1,13 @@
+#unfinushed
+#include "ft_printf.h"
+#include <stdarg.h>
+void ft_printf(char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    char *str, *val;
+    int tmp;
+    
+}
+
+
